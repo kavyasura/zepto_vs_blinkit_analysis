082ffa98f136b6e165777f83add7e6ae0f61e804
kavyaa_sura <22ce01036@iitbbs.ac.in>
2025-11-18 15:19:35 +0530

    Add SQL queries for various customer insights

---
+++ view_queries.sql
@@ -0,0 +1,39 @@
+-- top_5_customers_by_spend
+SELECT * FROM top_customers_by_spend;
+
+-- age_and_brand_wise_revenue
+SELECT * FROM age_brand_revenue;
+
+-- most_ordered_product_category_by_brand
+SELECT * FROM most_ordered_category_by_brand;
+
+-- age_group_wise_platform_revenue
+SELECT * FROM age_group_platform_revenue;
+
+-- top_3_selling_products_in_each_brand
+SELECT * FROM top_3_products_per_brand;
+
+-- product_categories_only_on_zepto_not_on_blinkit
+SELECT * FROM zepto_exclusive_categories;
+
+-- yearly_total_sales_by_each_brand
+SELECT * FROM yearly_sales_by_brand;
+
+-- top_3_cities_by_orders_per_brand
+SELECT * FROM top_3_cities_per_brand;
+
+-- bottom_3_states_by_revenue_per_brand
+SELECT * FROM least_performing_states;
+
+
+-- month_year_wise_revenue_comparison
+SELECT * FROM monthly_yearly_sales_comparison;
+
+-- total_weekend_sales_by_each_brand
+SELECT * FROM weekend_sales_by_brand;
+
+-- repeat_customers_ordering_more_than_3_times_per_brand
+SELECT * FROM repeat_orders_per_brand;
+
+-- total_repeat_orders_by_each_brand
+SELECT * FROM total_repeat_orders_summary;
